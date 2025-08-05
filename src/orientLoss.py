ca838770116fa5db7daedfc164ec1209dd824129
PsycheHalo <psyche@winsgod.com>
2025-08-05 20:26:00 +0800

    Update orientLoss.py

--- src/orientLoss.py
+++ src/orientLoss.py
@@ -16,6 +16,8 @@ def orientLoss(input,target,dim=1,meanOut=True,angleSmooth=1,normSmooth=1,dimSca
     loss1=(k+eps)**n
     loss2=(k-dot+eps)**m
     loss=loss1*loss2-(eps**(m+n))
+
+    loss=loss/(numel**dimScalingOrd)
     #loss[~torch.isfinite(loss)]=0
     
     if meanOut:
@@ -23,6 +25,7 @@ def orientLoss(input,target,dim=1,meanOut=True,angleSmooth=1,normSmooth=1,dimSca
     else:
         return loss
        
+
 
 
 
